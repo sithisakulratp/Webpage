9f437e9b2fc2c253674d5ed025303a0c5ccbcf87
Ploy Sithisakulrat <sithisakulratp@xavier.edu>
2017-05-03 23:01:22 -0400

    Update SamplePage.js

--- SamplePage.js
+++ SamplePage.js
@@ -1,74 +1,14 @@
+<!DOCTYPE html>
 <html>
-<head>
-<title>My Art(sy) Life Story</title>
-<style>
-body	{background-color: lightcyan}
-p	{text-indent: 50px}
-</style>
-</head>
-
 <body>
-<h1 align="center" style="color:midnightblue">My Art(sy) Life Story</h1>
-<h4 align="center"><i>created by Ploy Sithisakulrat</i></h4>
-<h2>Art Skills Development</h2>
-<p><font size="4">Art was just a hobby until I came to the United States. As a high school student, I started my
-	freshmen year with Foundation Art, which was a class for beginners. We learned basic stuff like lines, shapes,
-	how to shade, and how to do perspective. I liked it a lot, so I continued to take art class every year. During
-	my sophomore year, I started working on many media and styles. It was this year that I finally had a chance to
-	paint on wood and canvas, but then I did not do much od art my Junior year because I was taking two courses of art.
-	The assignment was overloaded. I did not like painting that much until my Senior year. I started to like painting
-	more than drawing. I took a higher course as year went by to develop my skills. In my senior year, I took a
-	highest level of art class, an AP Art. Because the time was rush, painting was the best solution to finish a project.
-	It is convenient and fast. During this time, I had many opportunities to submit many of my work for a couple
-	competitions outside of school. Even though I did not win anything, but I was already very happy that I got nominated.</font><p>
 
-<h2>My Artwork</h2>
-<p><font size="4">Below are some samples of my artwork. Because a concentration for my AP Art Portfolio was surrealism, there are many surrealistic art pieces.</font></p>
+<h2>Sample Webpage</h2>
 
-<img src="file:///Users/ploysitis/Downloads/rubikscubehead.jpg" style="width:270px"><img src="file:///Users/ploysitis/Downloads/cousins.jpg" style="width:270px"><img src="file:///Users/ploysitis/Downloads/universal.jpg" style="width:285px"><img src="file:///Users/ploysitis/Downloads/candle.jpg" style="width:220px"><img src="file:///Users/ploysitis/Downloads/stilllifecubism.jpg" style="width:238px">
+<button type="button"
+onclick="document.getElementById('demo').innerHTML = Date()">
+Click me to display Date and Time.</button>
 
-<img src="file:///Users/ploysitis/Downloads/pencilhands.jpg" style="width:425px"><img src="file:///Users/ploysitis/Downloads/rubikscube.jpg" style="width:425px"><img src="file:///Users/ploysitis/Downloads/sunset.jpg" style="width:425px">
+<p id="demo"></p>
 
-<img src="file:///Users/ploysitis/Downloads/siblings.jpg" style="width:270px"><img src="file:///Users/ploysitis/Downloads/underwater.jpg" style="width:480px"><img src="file:///Users/ploysitis/Downloads/mrstricker.jpg" style="width:270px">
-
-<h2>Styles of Art</h2>
-<p><font size="5">Below is a list of famous art movements that I have learned throughout my personal experiences.</font></p>
-<ul><font size="4">
-	<li>Impressionism</li>
-	<li>Post-Impressionism</li>
-	<li>Fauvism</li>
-	<li>Expressionism</li>
-	<li>Abstract</li>
-	<li>Cubism</li>
-	<li>Futurism</li>
-	<li>Suprematism</li>
-	<li>Constructivism</li>
-	<li>De Stijl</li>
-	<li>Realism</li>
-	<li>Surrealism</li>
-	<li>Neo Surrealism</li>
-	<li>Modernism</li>
-	<li>Spontaneous Realism</li>
-</font></ul>
-
-<h2>Inspirational Artists</h2>
-<table border>
-<tr>
-<td align="center">Name of Artist</td><td align="center">Style and Artwork</td><td align="center">Artwork<td align="center">Get to know them</td>
-</tr>
-<tr>
-<td align="center">Voka</td><td align="center">Spontaneous Realism</td><td><img src="http://www.sironafineart.com/sirona/wp-content/uploads/2014/03/jimihendrix_volka.jpg" style="width:250px"></td><td><a href="http://www.voka.at/en/">Here is a link to Voka's Website</a><br><br><a href="https://www.youtube.com/watch?v=f56WNu4T2aY">Check out this video</a></td>
-</tr>
-<tr>
-<td align="center">Salvador Dali</td><td align="center">Surrealism</td><td><img src="http://www.authenticsociety.com/Images/Atoms/Art/Painting/persistence_of_memory_dali_lowestres.jpg" style="width:280px"></td><td><a href="http://www.salvadordali.com/">Learn more about Dali</a></td>
-</tr>
-<tr>
-<td align="center">Van Gogh</td><td align="center">Impressionism</td><td><img src="http://www.davidpaulkirkpatrick.com/wp-content/uploads/2013/03/191-Van-Gogh-Starry-Nights-1-1024x640.jpg" style="width:280px"></td>
-<td><a href="http://www.vangoghgallery.com/">Learn more about Gogh</a></td>
-</tr>
-<tr>
-<td align="center">Pablo Picasso</td><td align="center">Cubism</td><td><img src="http://www.pablopicasso.org/images/paintings/three-musicians.jpg" style="width:250px"></td><td><a href="http://www.picasso.fr/us/picasso_page_index.php">Learn more about Picasso</a></td>
-</tr>
-</table>
-
-</html>
+</body>
+</html> 
